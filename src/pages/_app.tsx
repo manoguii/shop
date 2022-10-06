5c00e1b840ed7dfcbd5f6f24115af9f5853afcf7
Guilherme David <guilhermedavidrk@icloud.com>
2022-10-06 19:08:46 -0300

    :construction: Create Home

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,20 @@
 import { AppProps } from "next/app"
 import { globalStyles } from "../styles/global"
 
+import logo from '../assets/shop.svg'
+import { Container, Header } from "../styles/pages/app"
+import Image from "next/image"
 
 globalStyles()
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <Container>
+      <Header>
+        <Image src={logo.src} width={130} height={52} alt="" />
+      </Header>
+      <Component {...pageProps} />
+    </Container>
+  )
 }
 
