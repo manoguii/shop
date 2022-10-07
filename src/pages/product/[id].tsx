91750983f5ef447aebd72b00f5e7b2e17abc0916
Guilherme David <guilhermedavidrk@icloud.com>
2022-10-07 19:08:00 -0300

    :construction: Create styles component Product

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,9 +1,25 @@
 import { useRouter } from "next/router"
+import { ImageContainer, ProductContainer, ProductDetail } from "../../styles/pages/products"
 
 export default function Product() {
 
   const { query } = useRouter()
   return (
-    <h1>Product</h1>
-  )
+    <ProductContainer>
+      <ImageContainer>
+
+      </ImageContainer>
+
+      <ProductDetail>
+        <h1>camisetas X</h1>
+        <span>R$ 79,80</span>
+
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum vel reiciendis iure quam, quaerat et expedita, quasi ab veritatis aliquam unde inventore explicabo temporibus illo eum dolores rem modi nobis?</p>
+        
+        <button>
+          Comprar agora
+        </button>
+      </ProductDetail>
+    </ProductContainer>
+    )
 }
